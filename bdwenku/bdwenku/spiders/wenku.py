e2e4f76fc11a9bdcc91cb79014e8f2c9a8f5845b
13203930793 <zxc12350>
2021-11-04 14:23:55 +0800

    毕设第一次提交

---
+++ bdwenku/bdwenku/spiders/wenku.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class WenkuSpider(scrapy.Spider):
+    name = 'wenku'
+    allowed_domains = ['wenku.baidu.com']
+    start_urls = ['https://wenku.baidu.com/search?word=python&pn=1']
+
+    def parse(self, response):
+        print("Hello Scrapy")
+        # print(response.url)
+        # print(response.status)
+        alist=response.selector.css("dl dt a.tiaoquan")
+        print(len(alist))
+        for a in alist:
+            print(a.css("::attr(title)").get())
+        yield
+
+
+if __name__ == '__main__':
+    from scrapy import cmdline
+
+    args = "scrapy crawl wenku".split()
+    cmdline.execute(args)

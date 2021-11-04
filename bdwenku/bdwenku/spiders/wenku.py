8467de5bde4f8ac9657a14ee5713884bcc1d9360
13203930793 <zxc12350>
2021-11-04 14:25:42 +0800

    第二次测试

--- bdwenku/bdwenku/spiders/wenku.py
+++ bdwenku/bdwenku/spiders/wenku.py
@@ -10,7 +10,7 @@ class WenkuSpider(scrapy.Spider):
         print("Hello Scrapy")
         # print(response.url)
         # print(response.status)
-        alist=response.selector.css("dl dt a.tiaoquan")
+        alist = response.selector.css("dl dt a.tiaoquan")
         print(len(alist))
         for a in alist:
             print(a.css("::attr(title)").get())

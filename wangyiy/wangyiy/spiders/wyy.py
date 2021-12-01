13899d5ea1d9d8230ec93ca67a0a38c49b227625
13203930793 <zxc12350>
2021-12-01 12:52:20 +0800

    网易云用户百万歌单爬取改添加日志输出

--- wangyiy/wangyiy/spiders/wyy.py
+++ wangyiy/wangyiy/spiders/wyy.py
@@ -38,13 +38,14 @@ class WyySpider(scrapy.Spider):
     def user_information(self, response):
         json_text = response.text
         json_user = json.loads(json_text)["playlist"]
-        item=WangyiyItem()#实例化WangyiyItem
+        item = WangyiyItem()  # 实例化WangyiyItem
         for json_playlist in json_user:
-            item['playlistID']= str(json_playlist["id"])  # 播放列表ID
+            item['playlistID'] = str(json_playlist["id"])  # 播放列表ID
             print(item['playlistID'])
             item['name'] = json_playlist["name"]  # 歌单名字
             item['tags'] = "、".join(json_playlist["tags"])  # 标签
-            item['createTime'] = time.strftime("%Y-%m-%d", time.localtime(int(str(json_playlist["createTime"])[:-3])))  # 创建时间
+            item['createTime'] = time.strftime("%Y-%m-%d",
+                                               time.localtime(int(str(json_playlist["createTime"])[:-3])))  # 创建时间
             item['playCount'] = json_playlist["playCount"]  # 播放次数
             item['subscribedCount'] = json_playlist["subscribedCount"]  # 订阅计数
             item['nickname'] = json_playlist['creator']['nickname']  # 昵称
@@ -87,5 +88,9 @@ class WyySpider(scrapy.Spider):
 if __name__ == '__main__':
     from scrapy import cmdline
 
-    args = "scrapy crawl wyy --nolog".split()
+    args = "scrapy crawl wyy -s LOG_FILE=all.log".split()
     cmdline.execute(args)
+
+# scrapy crawl wyy --nolog不输出日志
+# scrapy crawl wyy默认输出日志到窗口
+# scrapy crawl wyy -s LOG_FILE=all.log #日志输出到当前目录下
